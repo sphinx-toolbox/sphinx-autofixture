90e8c15cc157acd366f8afc246fa9508bb0f210b
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-21 11:16:01 +0000

    Correctly handle locally defined functions in "is_fixture()" (#12)
    * Correctly handle locally defined functions in "is_fixture()"
    
    * Add tests of new dataclass handling.
    
    * Linting.

--- tests/test_directive.py
+++ tests/test_directive.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 from bs4 import BeautifulSoup  # type: ignore
 from domdf_python_tools.paths import PathPlus
-from domdf_python_tools.testing import max_version, min_version
+from domdf_python_tools.testing import min_version, only_version
 from pytest_regressions.file_regression import FileRegressionFixture
 from sphinx.testing.path import path
 from sphinx_toolbox.testing import check_html_regression
@@ -41,9 +41,15 @@ def original_datadir(request) -> Path:
 	return PathPlus(os.path.splitext(request.module.__file__)[0] + '_')
 
 
-@max_version("3.8", reason="Output differs on 3.6-3.7")
+@only_version("3.6", reason="Output differs on 3.6")
 @pytest.mark.parametrize("page", ["index.html"], indirect=True)
 def test_output_36(page: BeautifulSoup, file_regression: FileRegressionFixture):
+	check_html_regression(page, file_regression)
+
+
+@only_version("3.7", reason="Output differs on 3.7")
+@pytest.mark.parametrize("page", ["index.html"], indirect=True)
+def test_output_37(page: BeautifulSoup, file_regression: FileRegressionFixture):
 	check_html_regression(page, file_regression)
 
 
