8702bf92be7064350b8f88344367d30bb8d2f0f8
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-02-13 11:36:23 +0000

    Update for latest beautifulsoup

--- tests/test_directive.py
+++ tests/test_directive.py
@@ -1,11 +1,11 @@
 # stdlib
 import os
 from pathlib import Path
-from typing import Any, Iterator, Tuple, no_type_check
+from typing import Any, Iterator, Tuple, Union, cast, no_type_check
 
 # 3rd party
 import pytest
-from bs4 import BeautifulSoup, element  # type: ignore[import]
+from bs4 import BeautifulSoup, NavigableString, PageElement, Tag, element
 from coincidence import max_version, min_version, not_pypy, only_pypy, only_version
 from domdf_python_tools.paths import PathPlus
 from sphinx.application import Sphinx
@@ -84,21 +84,23 @@ def test_output(
 		version: str,
 		) -> None:
 
-	code: element.Tag
+	code: Union[PageElement, Tag, NavigableString]
 	for code in page.find_all("code", attrs={"class": "sig-prename descclassname"}):
-
+		assert isinstance(code, Tag)
 		first_child = code.contents[0]
 		if isinstance(first_child, element.Tag):
 			code.contents = [first_child.contents[0]]
 
 	for code in page.find_all("code", attrs={"class": "sig-name descname"}):
+		assert isinstance(code, Tag)
 		first_child = code.contents[0]
 		if isinstance(first_child, element.Tag):
 			code.contents = [first_child.contents[0]]
 
-	for div in page.findAll("script"):
+	for div in page.find_all("script"):
+		assert isinstance(div, Tag)
 		if div.get("src"):
-			div["src"] = div["src"].split("?v=")[0]
+			div["src"] = cast(str, div["src"]).split("?v=")[0]
 			print(div["src"])
 
 	html_regression.check(page, jinja2=True, jinja2_namespace={"alabaster_version": _get_alabaster_version()})
